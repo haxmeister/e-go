e047a415af97839e9126b3ea3e144f6ae972b821
Palica <palica+gitlab@liguros.net>
2019-11-19 09:49:39 +0100

    adding some go hacking to ego

---
+++ libs/query/query.go
@@ -0,0 +1,13 @@
+package query
+
+// Query document comment
+import "fmt"
+
+func Versions(option string) {
+	fmt.Println("querying versions of", option)
+}
+
+func Origin(option string) {
+	fmt.Println("querying versions of", option)
+}
+
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"gitlab.com/liguros/ego/libs/query"
+)
+
+type CommandArgs struct {
+	Sync struct {
+		Help bool
+	}
+	Query struct {
+		Versions string
+		Origin   string
+	}
+}
+
+func main() {
+
+	// create an instance of the struct called scArgs that will be a pointer to it
+	// this syntax creates a pointer.. very handy
+	scArgs := &CommandArgs{}
+
+	// create subcommand sync
+	syncCmd := flag.NewFlagSet("sync", flag.ExitOnError)
+	syncCmd.BoolVar(&scArgs.Sync.Help, "help", false, "display usage information")
+
+	// create subcommand query
+	queryCmd := flag.NewFlagSet("query", flag.ExitOnError)
+	queryCmd.StringVar(&scArgs.Query.Origin, "origin", "", "origin")
+	queryCmd.StringVar(&scArgs.Query.Versions, "versions", "", "versions")
+
+	// check
+	if len(os.Args) < 2 {
+		fmt.Println("Basic usage for ego:")
+		fmt.Println("supported subcommands are")
+		fmt.Println(" - sync")
+		fmt.Println(" - query")
+		fmt.Println("...")
+		os.Exit(1)
+	}
+
+	// look for the first argument from command line and dispatch accordingly
+	switch os.Args[1] {
+	case "sync":
+		syncCmd.Parse(os.Args[2:])
+		if scArgs.Sync.Help == true {
+			syncCmd.PrintDefaults()
+			//fmt.Println("Usage for ego sync")
+		} else {
+			sync()
+		}
+
+	case "query":
+		queryCmd.Parse(os.Args[2:])
+		if scArgs.Query.Versions != "" {
+			query.Versions(scArgs.Query.Versions)
+		} else if scArgs.Query.Origin != "" {
+			query.Origin(scArgs.Query.Origin)
+		} else {
+			os.Exit(1)
+		}
+
+	case "profile":
+		profile()
+
+	case "news":
+		news()
+
+	case "config":
+		config()
+
+	case "boot":
+		boot()
+	}
+
+}
+
+// Sync document comment
+func sync() {
+	fmt.Println("Syncing")
+}
+
+// Profile document comment
+func profile() {
+	fmt.Println("profiling...")
+}
+
+// News document comment
+func news() {
+	fmt.Println("news...")
+}
+
+// Config document comment
+func config() {
+	fmt.Println("config...")
+}
+
+// Boot document comment
+func boot() {
+	fmt.Println("boot...")
+}
+
